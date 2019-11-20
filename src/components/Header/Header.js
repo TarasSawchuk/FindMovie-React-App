1b179e93ed00698a9cd21742a85490cad0ad5d5b
TarasSawchuk <taras338lm@gmail.com>
2019-11-20 18:59:16 +0200

    third commit with some modules refactoring, styles, font adding

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,9 @@ import React from "react";
 const Header = (props) => {
     return (
         <header className="App-header">
-        <h2>{props.text}</h2>
+            <h2>Movie <span>4</span> evening</h2>
         </header>
-);
+    );
 };
 
 export default Header;
